44b6dc0b69304d8e467c42302a32e3a2347f678b
Anuradha <sangkeerthana@gmail.com>
2018-09-13 15:08:05 +0000

    Refreshing Js syntax

---
+++ JsSyntax.js
@@ -0,0 +1,14 @@
+const firstName = "Anu"
+const lastName = 'Seng'
+
+const val = 42
+const arr = ['string', 42, function() {console.log('Hi!')}, ]
+
+arr[2] ();
+
+for (let i = 0; i < arr.length; i++)
+{
+    console.log(arr[i]);
+}
+
+
