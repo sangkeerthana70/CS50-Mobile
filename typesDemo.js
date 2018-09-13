a09dc1fe2c7bf19e3a519e57861830856cc201a4
Anuradha <sangkeerthana@gmail.com>
2018-09-13 16:05:13 +0000

    More about JS

---
+++ typesDemo.js
@@ -0,0 +1,11 @@
+const x = 42;
+console.log(typeof x);
+
+
+const mySalary = 5560.99;
+console.log(typeof mySalary);
+
+const personName = "Jasmine";
+console.log(typeof personName);
+
+console.log(typeof null);
